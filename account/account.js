c2840a96191920745cecf3c46bafe3e35263d6f5
lexa <lexa@kali>
2026-05-03 00:25:58 +0300

    dashboard and sidebar

--- account/account.js
+++ account/account.js
@@ -1,4 +1,6 @@
 const date = document.getElementById('current-date');
+const navItems = document.querySelectorAll('.nav-item');
+const sections = document.querySelectorAll('main > section');
 
 function updateDate() {
   date.textContent = new Date().toLocaleDateString('uk-UA') + ' ' + new Date().toLocaleTimeString('uk-UA', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
@@ -9,4 +11,29 @@ if(window){
   setInterval(updateDate, 1000);
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+  const initialTarget = document.querySelector('.nav-item.active').getAttribute('data-target');
+  document.querySelector(`.${initialTarget}`).classList.add('active');
 
+  navItems.forEach(item => {
+    item.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      navItems.forEach(nav => nav.classList.remove('active'));
+      this.classList.add('active');
+
+      const targetClass = this.getAttribute('data-target');
+      
+      sections.forEach(section => {
+        section.classList.remove('active');
+        if (section.classList.contains(targetClass)) {
+          section.classList.add('active');
+        }
+      });
+    });
+  });
+});
+
+function animationSharpGlow(){
+  
+}
